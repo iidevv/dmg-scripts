66c285db2f185036b8f3c518ea122462b79adaab
Ilya Ilyich <mariomorales@Ilya-I.local>
2023-05-01 15:16:18 -0700

    dmg products

--- frontpage.js
+++ frontpage.js
@@ -1,53 +1,90 @@
-(function () {
-  var storeHash = "4n3dh09e13";
-  var categoryId = "416";
-  var container = document.getElementById("category-products");
+(async () => {
+  const container = document.getElementById("dmg-products");
 
-  fetch(
-    "https://warehouse.discountmotogear.com/bigcommerce-api/products?categories:in=416&limit=4",
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      var products = data.data;
-      products.forEach(function (product) {
-        console.log(product);
-        // var productDiv = document.createElement("div");
-        // productDiv.className = "Product";
+  try {
+    const response = await fetch(
+      "https://warehouse.discountmotogear.com/bigcommerce-api/products?categories:in=504&limit=4",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-        // var productTitle = document.createElement("h2");
-        // productTitle.className = "Product-title";
-        // productTitle.textContent = product.name;
-        // productDiv.appendChild(productTitle);
+    const data = await response.json();
+    const products = data.data;
 
-        // var productImage = document.createElement("div");
-        // productImage.className = "Product-image";
-        // var img = document.createElement("img");
-        // img.src = product.primary_image.url_standard;
-        // img.alt = product.name;
-        // productImage.appendChild(img);
-        // productDiv.appendChild(productImage);
+    const imagesData = await Promise.all(
+      products.map((product) =>
+        fetch(
+          `https://warehouse.discountmotogear.com/bigcommerce-api/products/${product.id}/images`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        ).then((res) => res.json())
+      )
+    );
 
-        // var productPrice = document.createElement("div");
-        // productPrice.className = "Product-price";
-        // productPrice.textContent = product.price.price;
-        // productDiv.appendChild(productPrice);
-
-        // var productLink = document.createElement("a");
-        // productLink.href = "/products/" + product.custom_url.url;
-        // productLink.className = "Product-link";
-        // productLink.textContent = "Read More";
-        // productDiv.appendChild(productLink);
-
-        // container.appendChild(productDiv);
-      });
-    })
-    .catch((error) => {
-      console.error("Error fetching products:", error);
+    const productsWithImages = products.map((product, index) => {
+      return {
+        ...product,
+        images: imagesData[index].data,
+      };
     });
+
+    productsWithImages.forEach((product) => {
+      const price =
+        product.sale_price > 0
+          ? `<div class="card-price" data-test-info-type="price">
+                    <div class="price-section price-section--withoutTax non-sale-price--withoutTax ">
+                        <span data-product-non-sale-price-without-tax="" class="price price--non-sale">
+                            $${product.price}
+                        </span>
+                    </div>
+                    <div class="price-section price-section--withoutTax">
+                        <span data-product-price-without-tax="" class="price price--withoutTax">
+                            $${product.sale_price}
+                        </span>
+                    </div>
+                </div>`
+          : `<div class="card-price" data-test-info-type="price">
+                                <div class="price-section price-section--withoutTax non-sale-price--withoutTax ">
+                                    <span data-product-non-sale-price-without-tax="" class="price">
+                                        $${product.price}
+                                    </span>
+                                </div>
+                            </div>`;
+      const productArticle = `
+                <article class="card" data-product-id="${product.id}">
+                <a href="${product.custom_url.url}" class="card-figure">
+                    <div class="themevale_badges">
+                        <div class="sale-badge themevale_badge">
+                            <span class="text">Sale</span>
+                        </div>
+                    </div>
+                    <img src="${product.images[0].url_standard}" alt="${product.name}"/>
+                </a>
+                <div class="card-body">
+                    <h4 class="card-title">
+                        <a href="${product.custom_url.url}" tabindex="0">${product.name}</a>
+                    </h4>
+                    <div class="card-wrapper">
+                        ${price}
+                    </div>
+                    <div class="card-figcaption">
+                    <a href="${product.custom_url.url}" data-event-type="product-click" class="button card-figcaption-button" data-product-id="${product.id}" tabindex="0">Choose Options</a>
+                    </div>
+                </div>
+                </article>
+            `;
+
+      container.insertAdjacentHTML("beforeend", productArticle);
+    });
+  } catch (error) {
+    console.error("Error fetching products:", error);
+  }
 })();
